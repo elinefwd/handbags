09bff463d2af67554c2ac2d5370338719bfd3273
elinefwd <elinefwd@gmail.com>
2023-11-26 21:46:41 +0100

    finished

---
+++ src/assets/components/button.jsx
@@ -0,0 +1,13 @@
+
+function Button({ text, disabled }) {
+    return (
+        <button
+            onClick={() => console.log(text)}
+            disabled={disabled}
+        >
+            {text}
+        </button>
+    );
+}
+
+export default Button;
---
+++ src/assets/components/tile.jsx
@@ -0,0 +1,13 @@
+
+function Tile({ image, imgDescription, title, children }) {
+    console.log(image, imgDescription, title, children)
+    return (
+        <section>
+            {image && <img src={image} alt={imgDescription} />}
+            <h2>{title}</h2>
+            {children}
+        </section>
+    );
+}
+
+export default Tile;
